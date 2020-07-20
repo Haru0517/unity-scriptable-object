4aa6e9742683ff080b65a3456ebf0201f18271b7
Yuto Chikazawa <chikazawa0517@gmail.com>
2020-07-20 14:52:19 +0900

    Add ScriptableObject sample

---
+++ Assets/FoodObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Create new food")]
+public class FoodObject : ScriptableObject
+{
+    public string foodName;
+    public int totalCalories;
+    public bool needsCooking;
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public FoodObject food;
+
+    void Start()
+    {
+        Debug.Log($"Name: {food.foodName}");
+        Debug.Log($"Calories: {food.totalCalories}");
+        Debug.Log($"needsCooking: {food.needsCooking}");
+    }
+}
